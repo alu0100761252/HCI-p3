55508c4adc919f5efc463f0332e14bdfff400976
alu0100761252 <alu0100761252@ull.edu.es>
2016-11-23 14:52:41 +0000

    Hecho todo menos angular que falla

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,14 +6,13 @@ function enviar(){
 
 }
 
+//cada vez que se envie algo a la BD se muestre por pantalla en el html el mensaje y el usuario asociado
 firebase.database().ref("Chat/").on('child_added', function(snapshot){
   var message = snapshot.val();
   displayChatMessage(message.name, message.text);
 })
 
-function displayChatMessage(name, text){
-
-
+function displayChatMessage(name, text){ 
   var historial = document.getElementById('chatbox');
   historial.innerHTML += "<div>"+ name + ": " +text+"</div>";
 }
